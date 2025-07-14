d7aea63e520bcd946bafa665d33467f3622e3a3a
Hank <love3357671@gmail.com>
2025-07-14 22:47:54 +0100

    [feat] update solution

--- solutions/array_and_list/s252/meeting_room.go
+++ solutions/array_and_list/s252/meeting_room.go
@@ -1,15 +1,23 @@
 package test
 
-func canAttendMeetings(intervals [][]int) bool {
-	hitMap := make(map[int]bool)
-	for _, interval := range intervals {
-		for i := interval[0]; i < interval[1]; i++ {
-			if val := hitMap[i]; val == true {
-				return false
-			}
-			hitMap[i] = true
-		}
+import "sort"
 
+func canAttendMeetings(intervals [][]int) bool {
+	if len(intervals) < 2 {
+		return true
+	}
+
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	prevEnd := intervals[0][1]
+	for i := 1; i < len(intervals); i++ {
+		start, end := intervals[i][0], intervals[i][1]
+		if start < prevEnd {
+			return false
+		}
+		prevEnd = end
 	}
 
 	return true
