6d455dc9ddcccde6facc6647f7e9ddeecb07390e
Hank <love3357671@gmail.com>
2025-07-16 21:02:23 +0100

    [feat] update it

--- solutions/sliding_window/s643/max_avg_subarray_1.go
+++ solutions/sliding_window/s643/max_avg_subarray_1.go
@@ -1,22 +1,21 @@
 package test
 
-import "math"
-
 func findMaxAverage(nums []int, k int) float64 {
-	result := math.MinInt
+	n := len(nums)
+	// 1) 算出第 1 個窗口的和，並設 maxSum = sum
 	sum := 0
-	left := 0
-	for right, val := range nums {
-		sum += val
-		if right-left+1 > k {
-			sum -= nums[left]
-			left++
-		}
+	for i := 0; i < k; i++ {
+		sum += nums[i]
+	}
+	maxSum := sum
 
-		if sum > result && right-left+1 == k {
-			result = sum
+	// 2) 窗口從 i=k…n-1 滑動：減掉離開的 + 加上進來的 → 更新 maxSum
+	for i := k; i < n; i++ {
+		sum += nums[i] - nums[i-k]
+		if sum > maxSum {
+			maxSum = sum
 		}
 	}
 
-	return float64(result) / float64(k)
+	return float64(maxSum) / float64(k)
 }
