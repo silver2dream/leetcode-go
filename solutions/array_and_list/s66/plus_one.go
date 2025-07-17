fb9e6195667527e9727b339513cf8a054ea0c36a
Hank <love3357671@gmail.com>
2025-07-17 16:58:07 +0100

    [feat] update it

--- solutions/array_and_list/s66/plus_one.go
+++ solutions/array_and_list/s66/plus_one.go
@@ -1,28 +1,28 @@
 package test
 
 func plusOne(digits []int) []int {
-	digit := 1
-	for i := len(digits) - 1; i >= 0; i-- {
-		digits[i] += digit
-		if digits[i] >= 10 {
-			digit = digits[i] / 10
-			digits[i] = 0
-		} else {
-			digit = 0
-			break
-		}
-	}
-
-	if digit > 0 {
-		result := []int{}
-		result = append(result, digit)
-		for _, v := range digits {
-			result = append(result, v)
-		}
-		return result
-	} else {
-		return digits
-	}
+	//digit := 1
+	//for i := len(digits) - 1; i >= 0; i-- {
+	//	digits[i] += digit
+	//	if digits[i] >= 10 {
+	//		digit = digits[i] / 10
+	//		digits[i] = 0
+	//	} else {
+	//		digit = 0
+	//		break
+	//	}
+	//}
+	//
+	//if digit > 0 {
+	//	result := []int{}
+	//	result = append(result, digit)
+	//	for _, v := range digits {
+	//		result = append(result, v)
+	//	}
+	//	return result
+	//} else {
+	//	return digits
+	//}
 
 	n := len(digits)
 	for i := n - 1; i >= 0; i-- {
